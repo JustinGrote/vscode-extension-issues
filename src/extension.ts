84a2938ae91d241747056e4819b7fa412f93cbe5
Justin Grote <JustinGrote@users.noreply.github.com>
2024-11-01 12:51:24 -0700

    Demonstrate Trace Issue

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,24 @@
-import { ExtensionContext, window } from "vscode";
+import { ExtensionContext, LogOutputChannel, window } from "vscode";
 
 export function activate(context: ExtensionContext) {
-	// Reproduction Goes Here then Launch "Run Extension [Temp Profile]"
+	const log = window.createOutputChannel("Log", { log: true });
+	window.showInformationMessage("Log Level startup is " + log.logLevel);
+	doLogs(log);
+
+	// On change log level, show what actually shows
+	log.onDidChangeLogLevel((level) => {
+		window.showInformationMessage("Log Level changed event (" + level + ") actual is " + log.logLevel);
+		log.appendLine("Log Level changed to " + level);
+		doLogs(log)
+	}, undefined, context.subscriptions);
+}
+
+
+function doLogs(log: LogOutputChannel) {
+	log.appendLine("AppendLine: Log Level is " + log.logLevel)
+	log.trace("Trace: Log Level is " + log.logLevel)
+	log.debug("Debug: Log Level is " + log.logLevel)
+	log.info("Info: Log Level is " + log.logLevel)
+	log.warn("Warn: Log Level is " + log.logLevel)
+	log.error("Error: Log Level is " + log.logLevel)
 }
