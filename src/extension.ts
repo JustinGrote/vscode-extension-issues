e9c2f64ecfceaf5f74bb3b919f03f28f43568c57
Justin Grote <JustinGrote@users.noreply.github.com>
2024-11-01 13:24:35 -0700

    Race Condition

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,10 @@
 import { ExtensionContext, window } from "vscode";
 
 export function activate(context: ExtensionContext) {
-	// Reproduction Goes Here then Launch "Run Extension [Temp Profile]"
+	const log = window.createOutputChannel("Log", {log: true})
+	const log2 = window.createOutputChannel("Log", {log: true})
+	context.subscriptions.push(log, log2)
+	log.appendLine("Log1")
+	log2.appendLine("Log2")
+	log.appendLine("Log1")
 }
